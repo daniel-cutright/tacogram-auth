dae76a26fc13b378ed38dc4e6fcd9fd8fccb45e3
daniel-cutright <daniel.cutright@kellogg.northwestern.edu>
2026-02-28 15:28:44 +0000

    Completed posts challenge

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,14 +6,23 @@ class PostsController < ApplicationController
   end
 
   def new
+    # Redirect if user is not logged in
+    if @current_user == nil
+      flash["notice"] = "Please login to submit a post."
+      redirect_to "/login"
+    end
   end
   
   def create
-    @post = Post.new
-    @post["body"] = params["body"]
-    @post["image"] = params["image"]
-    @post["user_id"] = session["user_id"]
-    @post.save
+    if @current_user != nil
+      @post = Post.new
+      @post["body"] = params["body"]
+      @post["image"] = params["image"]
+      @post["user_id"] = session["user_id"]
+      @post.save
+    else
+      flash["notice"] = "Please login to submit a post."
+    end
     redirect_to "/posts"
   end
 end
